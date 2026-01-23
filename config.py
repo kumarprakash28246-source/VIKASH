26ab73dba80fbe414c7b5492cc844b7915b1919d
kumarprakash28246-source <kumarprakash28246@gmail.com>
2026-01-24 05:21:40 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "8565057360"))
+OWNER_ID = int(getenv("OWNER_ID", "6225033257"))
 
 LOG = int(getenv("LOG", True))
 
@@ -48,8 +48,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GIRLS_BOYS_DP_STYLISH")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/GIRLS_BOYS_DP_STYLISH")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KASHIKA_MUSIC_UPDATE")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KASHIKA_MUSIC_UPDATE")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
